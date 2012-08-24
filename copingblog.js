b7367d474d39c2ab3cba58af469950d08197a216
Andrea Cantieni PHZ <andrea.cantieni@phz.ch>
2012-08-24 09:58:20 +0200

    dev

--- copingblog.js
+++ copingblog.js
@@ -11,6 +11,34 @@ function display_visi()
 		document.getElementById("post_password").style.fontWeight = "bold"
 		
 	}
+
 }
 
-window.onload = display_visi
+//window.onload = display_visi
+
+jQuery(document).ready(function() {
+	
+	display_visi()
+	
+	//collaps admin menu on the left side
+	jQuery(document.body).addClass("folded");
+	setUserSetting("mfold","f")
+
+/*    
+	//experimental: ponter box to indicate password field
+	jQuery('#post_password').pointer({
+        content: '<h3>Passwort</h3><p>Hier wird das Passwort gesetzt</p>',
+        position: {
+					my: 'right right',
+					at: 'left right',
+					offset: '0 0'
+				},
+        close: function() {
+            // Once the close button is hit
+        }
+      }).pointer('open');
+*/ 
+
+
+});
+
