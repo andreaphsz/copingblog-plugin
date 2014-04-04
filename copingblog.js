8fbb19fe3b41f812149d467d9c84c1c087ea59c6
andreaphsz <andrea.cantieni@phsz.ch>
2014-04-04 20:07:00 +0200

    big changes

--- copingblog.js
+++ copingblog.js
@@ -1,8 +1,51 @@
-function display_visi()
+jQuery(document).ready(function() {
+	
+var getUrlParameters = function(parameter, staticURL, decode){
+   /*
+    Function: getUrlParameters
+    Description: Get the value of URL parameters either from 
+                 current URL or static URL
+    Author: Tirumal
+    URL: www.code-tricks.com
+   */
+   var currLocation = (staticURL.length)? staticURL : window.location.search,
+       parArr = currLocation.split("?")[1].split("&"),
+       returnBool = true;
+   
+   for(var i = 0; i < parArr.length; i++){
+        parr = parArr[i].split("=");
+        if(parr[0] == parameter){
+            return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+            returnBool = true;
+        }else{
+            returnBool = false;            
+        }
+   }
+   
+   if(!returnBool) return false;  
+}
+
+var display_visi = function ()
 {
 	document.getElementById("post-visibility-select").style.display = "block"
 
 	var x = document.getElementById("post_password")
+	var y =  document.getElementById("visibility-radio-password")
+	var z =  document.getElementById("visibility-radio-private")
+	var visi = getUrlParameters("visi", "", true);
+	var form = getUrlParameters("form", "", true);
+	/*
+	if (visi=="private")
+	{
+		z.checked=true
+	}
+	
+	if (x.value == "" & visi=="pwd")
+	{
+		x.value="1234"
+		y.checked=true
+	}
+	*/
 	if (x.value != "")
 	{
 		x.style.border = "solid red 2px"
@@ -11,18 +54,41 @@ function display_visi()
 		document.getElementById("post_password").style.fontWeight = "bold"
 		
 	}
-
+	
+	if (form == "reflex")
+	{
+		//tinymce.activeEditor.setContent("<h2>Reflexion</h2>")
+	}
+	
+	
 }
+
+jQuery("#a_ps1").click(function(){
+  jQuery("#ps1").toggle();
+});
+jQuery("#a_ps2").click(function(){
+  jQuery("#ps2").toggle();
+});
+
+jQuery("#neu_ps1").click(function(){
+	var lastid=jQuery("#ps1 div").last().attr('id')
+	var j=1
+	//var i=2+1
+	var i= 1+parseInt(lastid.substring(lastid.search("_")+3))
+	var ji=j+"_"+i
+	//jQuery("#neu_ps1").before("<p>hallo</p>"+lastid)
+	jQuery("#neu_ps1").before("<div id='ps_"+ji+"' style='border-bottom:1px solid;margin-left:20px;'><p>Datum:<input type='text1_"+ji+"'></p><p>Zielebene:</p><textarea name='text2_"+ji+"' cols='60' rows='5'></textarea><br><p>Konsequenzen:</p><textarea name='text3_"+ji+"' cols='60' rows='5'>hallo</textarea><br></div>")
+})
 
 //window.onload = display_visi
 
-jQuery(document).ready(function() {
-	
+
+	//window.setTimeout(display_visi(), 10000);
 	display_visi()
 	
 	//collaps admin menu on the left side
-	jQuery(document.body).addClass("folded");
-	setUserSetting("mfold","f")
+	//jQuery(document.body).addClass("folded");
+	//setUserSetting("mfold","f")
 
 /*    
 	//experimental: ponter box to indicate password field
--- copingblog.php
+++ copingblog.php
@@ -9,30 +9,108 @@ Author URI: http://schwyz.phz.ch
 License: GPL2
 */
 
+add_filter('default_content', 'my_editor_content');
+
+function my_editor_content( $content ) {
+	
+	$new_ps = isset($_GET["new_ps"]) ? $_GET["new_ps"] : false;
+	
+	if ($new_ps != false) {
+		$content = "Datum: <br> Zielebene: <br> Konsequenzen: <br>" ;
+	}
+	
+	return $content;
+}
+
+add_filter('default_title', 'my_editor_title');
+
+function my_editor_title() {
+	$new_ps = isset($_GET["new_ps"]) ? $_GET["new_ps"] : false;
+	
+	if ($new_ps != false) {
+		return $new_ps . ": mein schöner titel";
+	}
+}
+
+add_action('save_post', 'my_save_post');
+
+function my_save_post( $post_id ){
+	$new_ps = isset($_GET["new_ps"]) ? $_GET["new_ps"] : false;
+	
+	if ($new_ps != false) {
+		$ps = get_cat_ID( $new_ps );
+		wp_set_post_categories( $post_id, array($ps));
+	}
+}
+
+function display_feinplanung() {
+	echo "link zum fragebogen";
+}
+
+function display_reflexion() {
+	$cat_ids =  get_cat_ID('ps1').",".get_cat_ID('ps2');
+	$ps_count = get_categories( array('include'=> $cat_ids) ); 
+	
+	echo "<h3>PS 1 versteht und vermittelt Fachinhalte</h3>";
+	echo "<a href='edit.php?post_status=all&post_type=post&cat=".get_cat_ID( 'ps1' )."&paged=1&mode=excerpt'>anzeigen (".$ps_count[0]->count.")</a><br>";
+	echo "<a href='post-new.php?new_ps=ps1'>neu</a><br><br>";
+	echo "<h3>PS 2 ...</h3>";
+	echo "<a href='edit.php?post_status=all&post_type=post&cat=".get_cat_ID( 'ps2' )."&paged=1&mode=excerpt'>anzeigen (".$ps_count[1]->count.")</a><br>";
+	echo "<a href='post-new.php?new_ps=ps2'>neu</a><br><br>";
+	
+}
+
+function menu_copingblog() {
+	//include_once('reflexion.class.php');
+	//$reflexion = new Reflexion();
+	//$reflexion->init();
+	add_menu_page( "Feinplanung", "Feinplanung", "read", "feinplanung_menu", 'display_feinplanung');
+	add_submenu_page( "feinplanung_menu", "Reflexion", "Reflexion", "read", "reflexion_menu", 'display_reflexion');
+}
+add_action('admin_menu', 'menu_copingblog');
+
 add_action('save_post','set_visibility', 10, 2);
 
 function set_visibility($post_ID, $post)
 {
 	global $wpdb;
+	/*
+	$form = isset($_GET["form"]) ? $_GET["form"] : "";
 	
+	if ($form == "reflex")
+	{
+		$wpdb->update( $wpdb->posts, array( 'post_content' => "<h2>Reflexion</h2>" ), array( 'ID' => $post_ID ) );
+		$wpdb->update( $wpdb->posts, array( 'post_title' => "Reflexion" ), array( 'ID' => $post_ID ) );
+	}
+	*/
 	$visi = isset($_GET["visi"]) ? $_GET["visi"] : "public";
 
-	if ($visi == "pwd") 
-	{
-		$wpdb->update( $wpdb->posts, array( 'post_password' => rand(1000,9999) ), array( 'ID' => $post_ID ) );
-	}
+	if ( ! wp_is_post_revision( $post_ID ) ) { //see https://codex.wordpress.org/Function_Reference/wp_update_post
+		// unhook this function so it doesn't loop infinitely
+		remove_action('save_post', 'set_visibility');
+		
+		if ($visi == "pwd") 
+		{
+			// update the post, which calls save_post again
+			wp_update_post( array( 'post_password' => rand(1000,9999), 'ID' => $post_ID ) );
+		}
 
-	if ($visi == "private") 
-	{
-		$wpdb->update( $wpdb->posts, array( 'post_status' => 'private' ), array( 'ID' => $post_ID ) );
+		if ($visi == "private") 
+		{
+			wp_update_post( array( 'post_status' => 'private', 'ID' => $post_ID ) );
+		}
+	
+		// re-hook this function
+		add_action('save_post', 'set_visibility');
 	}
+	
 }
 
 add_action( 'admin_enqueue_scripts', 'my_enqueue', 10, 1 );
 
 function my_enqueue($hook) {
-    if( 'post-new.php' != $hook && 'post.php' != $hook)
-       return;
+    //if( 'post-new.php' != $hook && 'post.php' != $hook)
+    //   return;
     wp_enqueue_script( 'my_custom_script', plugins_url('/copingblog.js', __FILE__) );
 
 }
